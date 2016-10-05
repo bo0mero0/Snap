5a1c909a12a03ba19efb398ac115872f96ebe21e
bo0mero0 <pbtruong@gmail.com>
2016-10-05 18:04:35 -0400

    fix header css

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginMenu from './login_menu/login_menu_container'
 import SignupContainer from './signup/signup_container';
+import { Link } from 'react-router';
 
 const App = ({ children }) => (
   <div className="app">
     <header className="header group">
-      <h1> Snap </h1>
+      <h1><Link to="/" >Snap</Link></h1>
       <ul className="navbar">
         <LoginMenu />
       </ul>
