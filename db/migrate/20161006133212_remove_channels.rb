127611d778bca5ff990348e3b64aede9a161081b
bo0mero0 <pbtruong@gmail.com>
2016-10-09 16:36:27 -0400

    delete migrate

--- db/migrate/20161006133212_remove_channels.rb
+++
@@ -1,5 +0,0 @@
-class RemoveChannels < ActiveRecord::Migration
-  def change
-    drop_table :channels
-  end
-end
--- db/migrate/20161006133324_drop_channels.rb
+++
@@ -1,5 +0,0 @@
-class DropChannels < ActiveRecord::Migration
-  def change
-    drop_table :channels
-  end
-end
