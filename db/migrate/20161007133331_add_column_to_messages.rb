c9da9672cd05b0e1ebcbff10ce9ccc7621a4c28a
bo0mero0 <pbtruong@gmail.com>
2016-10-09 16:42:13 -0400

    delete unwanted migration

--- db/migrate/20161007133331_add_column_to_messages.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :body, :string, null: false
-    add_column :messages, :author_id, :integer, null: false
-    add_column :messages, :channel_id, :integer, null: false
-    add_index :messages, :author_id
-    add_index :messages, :channel_id 
-  end
-end
--- db/migrate/20161007133640_add_time_stamp_column_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeStampColumnToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :created_at, :datetime
-    add_column :messages, :updated_at, :datetime
-  end
-end
