16ce71a91cb69c0ea8f014369b8b71f38db2ab9f
bo0mero0 <pbtruong@gmail.com>
2016-10-09 16:24:08 -0400

    fix syntax error in channel Subscription.rb

--- app/models/channel_subscription.rb
+++ app/models/channel_subscription.rb
@@ -1,6 +1,6 @@
 class ChannelSubscription < ActiveRecord::Base
   belongs_to :channel
-  belongs_to :user,
+  belongs_to :user
 
   validates :user, :channel, presence: true
   validates_uniqueness_of :user, scope: :channel
